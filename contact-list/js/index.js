f9e21f99c9d2e23fa2ecd533c6b506c9e9fda9f9
Ilya Kyazimov <40570012+BruceWayne3@users.noreply.github.com>
2018-12-19 18:51:09 +0300

    Update index.js

--- contact-list/js/index.js
+++ contact-list/js/index.js
@@ -1,23 +1,15 @@
 'use strict';
 const contacts = document.querySelector('.contacts-list');
-contacts.innerHTML = ''; 
 try {
 	const downloadedContacts = JSON.parse(loadContacts());
 } catch (e) {
 	console.error (e.name, e.message);
 };
 
-function showContacts(downloadedContacts) {
-	let newContacts;
-	let i;
-	for (const downloadedContact of downloadedContacts) {  
-		contacts.appendChild(document.createElement('li'));
-		newContacts = document.querySelectorAll('.contacts-list > li'); 
-		i = downloadedContacts.indexOf(downloadedContact); 
-		newContacts[i].innerHTML =`<strong> ${downloadedContact.name} </strong>`;	 
-		newContacts[i].dataset.phone = downloadedContact.phone; 
-		newContacts[i].dataset.email = downloadedContact.email;	
-	}
-}
+let data = '';
 
-showContacts(downloadedContacts);
+downloadedContacts.forEach(contact => {
+  data += `<li data-email="${contact.email}" data-phone="${contact.phone}"><strong>${contact.name}</strong></li>`
+});
+
+contacts.innerHTML = data;
